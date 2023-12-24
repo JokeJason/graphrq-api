ed86524f3f1786fcf120f350b4937b1b950a170a
Yiqing Zhu <793831308@qq.com>
2023-12-24 18:03:41 +1100

    Use schema.graphql to replace typeDefs.ts

--- src/newServer.ts
+++ src/newServer.ts
@@ -1,9 +1,15 @@
 import neo4j from 'neo4j-driver';
+import { readFileSync } from 'fs';
+import path from 'path';
 import { Neo4jGraphQL } from '@neo4j/graphql';
 import { ApolloServer } from '@apollo/server';
 import 'dotenv/config.js';
+import {fileURLToPath} from "node:url";
 
-import { typeDefs } from './typeDefs.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const schemaPath = path.join(__dirname, 'schema.graphql');
+const typeDefs = readFileSync(schemaPath).toString('utf-8');
 
 export const driver = neo4j.driver(
   process.env.NEO4J_URI,
