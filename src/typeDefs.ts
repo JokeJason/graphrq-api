45f94a51a860e5842e7df7973b05c8763f97f569
Jason Zhu <jasonzhuyq@outlook.com>
2023-12-30 23:51:34 +1100

    Import upperDirectiveTypeDefs into typeDefs.ts

--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,69 +1,72 @@
 import { gql } from 'graphql-tag';
+import { upperDirectiveTypeDefs } from './directives/upperDirective.js';
 
 // Here is where we define data types of our Neo4j database. By using this type definition, Neo4jGraphQL library will
 // automatically generate GraphQL schema for us (which can be retrieved in Apollo Sandbox).
 // And because we are not defining a complete GraphQL DSL, we don't use .graphql file.
 
-export const typeDefs = gql`
-  directive @uppercase on FIELD_DEFINITION
-  type User {
-    id: ID! @id
-    name: String! @uppercase
-    email: String! @unique
-  }
+export const typeDefs = [
+  upperDirectiveTypeDefs,
+  gql`
+    type User {
+      id: ID! @id
+      name: String! @uppercase
+      email: String! @length(max: 8) @unique
+    }
 
-  enum RequirementCategory {
-    CUSTOMER
-    SYSTEM
-    ENGINEERING
-    USER_STORY
-    IMPLEMENTATION
-    QUALITY_ASSURANCE
-  }
+    enum RequirementCategory {
+      CUSTOMER
+      SYSTEM
+      ENGINEERING
+      USER_STORY
+      IMPLEMENTATION
+      QUALITY_ASSURANCE
+    }
 
-  type Requirement {
-    id: ID! @id
-    name: String!
-    description: String
-    createdAt: DateTime! @timestamp(operations: [CREATE])
-    creator: User @relationship(type: "CREATED", direction: IN)
-    updatedAt: DateTime! @timestamp(operations: [UPDATE])
-    parent: Requirement @relationship(type: "CHILD_OF", direction: OUT)
-    children: [Requirement!]! @relationship(type: "CHILD_OF", direction: IN)
-    tests: [Test!]! @relationship(type: "TESTED_BY", direction: IN)
-  }
+    type Requirement {
+      id: ID! @id
+      name: String!
+      description: String
+      createdAt: DateTime! @timestamp(operations: [CREATE])
+      creator: User @relationship(type: "CREATED", direction: IN)
+      updatedAt: DateTime! @timestamp(operations: [UPDATE])
+      parent: Requirement @relationship(type: "CHILD_OF", direction: OUT)
+      children: [Requirement!]! @relationship(type: "CHILD_OF", direction: IN)
+      tests: [Test!]! @relationship(type: "TESTED_BY", direction: IN)
+    }
 
-  enum TestCategory {
-    UNIT_TEST
-    INTEGRATION_TEST
-    SYSTEM_TEST
-  }
+    enum TestCategory {
+      UNIT_TEST
+      INTEGRATION_TEST
+      SYSTEM_TEST
+    }
 
-  enum TestStatus {
-    PASSED
-    FAILED
-    PENDING
-    IN_PROGRESS
-  }
+    enum TestStatus {
+      PASSED
+      FAILED
+      PENDING
+      IN_PROGRESS
+    }
 
-  type TestResult {
-    id: ID!
-    passed: Boolean!
-    details: String
-    url: String!
-    timestamp: DateTime! @timestamp(operations: [CREATE])
-  }
+    type TestResult {
+      id: ID!
+      passed: Boolean!
+      details: String
+      url: String!
+      timestamp: DateTime! @timestamp(operations: [CREATE])
+    }
 
-  type Test {
-    id: ID! @id
-    name: String!
-    description: String
-    category: TestCategory!
-    status: TestStatus!
-    createdAt: DateTime! @timestamp(operations: [CREATE])
-    updatedAt: DateTime! @timestamp(operations: [UPDATE])
-    creator: User @relationship(type: "CREATED", direction: IN)
-    requirement: Requirement @relationship(type: "TESTED_BY", direction: OUT)
-    results: [TestResult!]! @relationship(type: "TEST_RESULT", direction: OUT)
-  }
-`;
+    type Test {
+      id: ID! @id
+      name: String!
+      description: String
+      category: TestCategory!
+      status: TestStatus!
+      createdAt: DateTime! @timestamp(operations: [CREATE])
+      updatedAt: DateTime! @timestamp(operations: [UPDATE])
+      creator: User @relationship(type: "CREATED", direction: IN)
+      requirement: Requirement @relationship(type: "TESTED_BY", direction: OUT)
+      results: [TestResult!]! @relationship(type: "TEST_RESULT", direction: OUT)
+    }
+  `,
+];
